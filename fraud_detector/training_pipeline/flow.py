f594d7751764b8ab77530938c210c640e29ef146
ravil-gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2025-06-30 15:31:42 +0400

    minor fixes

--- fraud_detector/training_pipeline/flow.py
+++ fraud_detector/training_pipeline/flow.py
@@ -2,11 +2,11 @@ import os
 
 from prefect import flow
 
+from fraud_detector.config import TEST_PATH, TRAIN_PATH
 from fraud_detector.training_pipeline.tasks import (
     eval_model_on_test,
     load_model_from_model_registry,
-    load_test_X_y,
-    load_train_X_y,
+    load_X_y,
     register_model,
     train_model,
 )
@@ -16,8 +16,8 @@ METRIC_THRESHOLD = os.environ["METRIC_THRESHOLD"]
 
 @flow
 def train_flow():
-    X, y = load_train_X_y()
-    test_X, test_y = load_test_X_y()
+    X, y = load_X_y(data_path=TRAIN_PATH)
+    test_X, test_y = load_X_y(data_path=TEST_PATH)
 
     model = load_model_from_model_registry()
 
@@ -25,5 +25,5 @@ def train_flow():
 
     metric = eval_model_on_test(model=model, test_X=test_X, test_y=test_y)
 
-    if metric <= METRIC_THRESHOLD:
+    if metric >= METRIC_THRESHOLD:
         register_model(model=model, model_name="production_model")
--- fraud_detector/training_pipeline/tasks.py
+++ fraud_detector/training_pipeline/tasks.py
@@ -1,14 +1,21 @@
+import pandas as pd
 from prefect import task
+from sklearn.metrics import f1_score
+
+from fraud_detector.config import (
+    FEATURE_COLUMNS,
+    TARGET_COLUMN,
+)
 
 
 @task
-def load_train_X_y():
-    pass
+def load_X_y(data_path):
+    data = pd.read_csv(data_path)
 
+    X = data[FEATURE_COLUMNS]
+    y = data[TARGET_COLUMN]
 
-@task
-def load_test_X_y():
-    pass
+    return X, y
 
 
 @task
@@ -18,12 +25,21 @@ def load_model_from_model_registry():
 
 @task
 def train_model(model, X, y):
-    pass
+    model.fit(X, y)
+
+    return model
 
 
 @task
 def eval_model_on_test(model, test_X, test_y):
-    pass
+    predictions = model.predict(test_X)
+
+    metric = f1_score(
+        y_true=test_y,
+        y_pred=predictions,
+    )
+
+    return metric
 
 
 @task
