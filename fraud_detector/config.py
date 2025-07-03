d5058fc35e10c8f7fe44e61602f18b65564fd94b
ravil-gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2025-07-03 23:00:30 +0400

    added config vars

--- fraud_detector/config.py
+++ fraud_detector/config.py
@@ -1,3 +1,5 @@
+import os
+
 FEATURE_COLUMNS = ["V4", "V11", "V7", "Amount"]
 TARGET_COLUMN = "Class"
 
@@ -6,7 +8,9 @@ TEST_PATH = "data/test.csv"
 
 RANDOM_STATE = 42
 
-MLFLOW_TRACKING_URI = "http://localhost:5000"
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
 MLFLOW_EXPERIMENT_NAME = "fraud_detection_experiment"
+
+PREFECT_API_URL = os.getenv("PREFECT_API_URL", "http://localhost:4200/api")
 
 METRIC_THRESHOLD = 0.6  # Example threshold for F1 score
